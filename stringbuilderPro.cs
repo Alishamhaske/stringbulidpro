c43a52f7f47630c85516a0361416eb36fb3a9476
Alishamhaske <mmhaske2001@gmail.com>
2023-08-29 12:55:51 +0530

    Add project files.

---
+++ stringbuilderPro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopfirstclass_program.stringbuildpro
+{
+    internal class stringbuilderPro
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "hello";
+            Console.WriteLine(str1);
+            Console.WriteLine(str1.GetHashCode());
+            str1 = str1 + "Good Morning";
+            Console.WriteLine(str1);
+            Console.WriteLine(str1.GetHashCode());
+
+
+
+            Console.WriteLine("string bulider ");
+            StringBuilder str2 = new StringBuilder("hello");
+
+            Console.WriteLine(str2);
+            Console.WriteLine(str2.GetHashCode());
+            /* strbuilder.Append("Good Morning");
+             Console.WriteLine(strbuilder);
+             Console.WriteLine(strbuilder.GetHashCode());*/
+            str2.Append("good afternoon");
+            Console.WriteLine(str2);
+            Console.WriteLine(str2.GetHashCode());
+
+
+
+        }
+    }
+}
---
+++ stringbulid1.cs
@@ -0,0 +1,41 @@
+﻿using oopfirstclass_program.arraylistcollection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopfirstclass_program.stringbulidpro
+{
+    internal class stringbulid1
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder str2 = new StringBuilder("Hello to all");
+            Console.WriteLine(str2);
+
+            str2.Append("Good Morning");
+            Console.WriteLine(str2);
+
+            str2.Remove(4, 5);
+            Console.WriteLine(str2);
+
+            //str2.Replace('l', 'L');
+            //Console.WriteLine(str2);
+
+            str2.Replace("all", "everyone");
+            Console.WriteLine(str2);
+
+            Console.WriteLine("Capacity =" + str2.Capacity);
+
+            str2.Clear();// remove all the letters / strbuilder will be empty
+
+            Console.WriteLine(str2);
+
+         
+            
+
+        }
+    }
+}
